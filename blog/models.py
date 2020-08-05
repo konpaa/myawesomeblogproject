bd18f1b0d2b5931ca2518fbc3dd6c8b2668da362
Pavel <pashakonoplyanikov@gmail.com>
2020-08-05 15:00:43 +0300

    post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+	post_title=models.CharField(max_length=300)
+	post_image=models.ImageField(upload_to='event_images/')
+	post_text=models.TextField()
+	post_data=models.DateTimeField()
